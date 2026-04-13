da4e42ce90aba833277238dcf73d2c6202a8b61f
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-04-13 03:21:48 -0700

    css coding for RelatedProduct.jsx file

--- Frontend/src/Pages/Products.jsx
+++ Frontend/src/Pages/Products.jsx
@@ -4,6 +4,7 @@ import BredCrums from '../Components/BredCrums';
 import ProductDisplay from '../Components/ProductDisplay';
 import { useParams } from 'react-router-dom';
 import Descriptions from '../Components/Descriptions';
+import RelatedProduct from '../Components/RelatedProduct';
 
 const Products = () => {
   const {all_product} = useContext(ShopContext);
@@ -14,6 +15,7 @@ const product = all_product.find((e) => e.id === Number(productId))
       <BredCrums product={product} />
       <ProductDisplay product={product}/>
       <Descriptions/>
+      <RelatedProduct/>
     </div>
   )
 }
