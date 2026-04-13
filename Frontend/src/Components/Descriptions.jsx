96872b2c2a5c62a5b40a356952a9f88fffc969b6
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-04-13 02:25:20 -0700

    css coding for description.jsx file

--- Frontend/src/Components/Descriptions.jsx
+++ Frontend/src/Components/Descriptions.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Descriptions = () => {
   return (
-    <div>
-      <div>
-        <p>Description</p>
-        <p>Review(135)</p>
+    <div className=' mx-15 lg:mx-40 my-10'>
+      <div className='flex font-semibold text-black-400'>
+        <p className='border border-gray-500 px-5 py-1'>Description</p>
+        <p className='border border-gray-500 px-5 py-1'>Review(135)</p>
       </div>
-      <div>
-        <p>
+      <div className='border border-gray-500 p-5'>
+        <p className='text-black-400 font-semibold'>
             You can find various styles of half-star icons in PNG format on these popular platforms: UXWing: Offers a clean yellow half-star icon available for download in PNG (up to 512px) and SVG formats.You can find various styles of half-star icons in PNG format on these popular platforms: UXWing: Offers a clean yellow half-star icon available for download in PNG (up to 512px) and SVG formats.You can find various styles of half-star icons in PNG format on these popular platforms: UXWing: Offers a clean yellow half-star icon available for download in PNG (up to 512px) and SVG formats.
         </p>
       </div>
