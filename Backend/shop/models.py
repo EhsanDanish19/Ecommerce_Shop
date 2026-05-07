5a3c2c7023076b53dba4981f35c1491604a0b256
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-05-06 23:24:22 -0700

    Updates

--- Backend/shop/models.py
+++ Backend/shop/models.py
@@ -1,7 +1,21 @@
 
 
+
+from turtle import back
+
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     CATEGORY_CHOICES =[
@@ -11,9 +25,12 @@ class Product(models.Model):
 
     ]
 
+    tags = models.ManyToManyField(Tag, blank=True)
+    sizes = models.ManyToManyField(Size, blank=True)
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description=models.TextField()
     new_price = models.FloatField()
     old_price = models.FloatField()
     is_popular = models.BooleanField(default=False)
--- Backend/shop/serializers.py
+++ Backend/shop/serializers.py
@@ -1,9 +1,21 @@
 from dataclasses import field
 
 from rest_framework import serializers
-from .models import Product
+from .models import *
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Tag
+        field='__all__'
+
+class SizeSerializser(serializers.ModelSerializer):
+
+    class Meta:
+        model = Size
+        field = '__all__'
+        
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
         fields = '__all__'
