5f629ed05ceee7e743a7d963793dc2beb68084a5
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-05-07 03:41:55 -0700

    Fetch cartItems data from the database

--- Backend/shop/serializers.py
+++ Backend/shop/serializers.py
@@ -3,19 +3,14 @@ from dataclasses import field
 from rest_framework import serializers
 from .models import *
 
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model =Tag
-        field='__all__'
-
-class SizeSerializser(serializers.ModelSerializer):
-
-    class Meta:
-        model = Size
-        field = '__all__'
-        
 class ProductSerializer(serializers.ModelSerializer):
+    tags = serializers.StringRelatedField(many=True)
+    sizes = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Product
         fields = '__all__'
+    def get_sizes(self, obj):
+        # obj.sizes is ManyToMany OR broken string case
+        if isinstance(obj.sizes, str):
+            return [s.strip() for s in obj.sizes.split(",")]
