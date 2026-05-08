54650c50bfae6a0f6e594d7d617596acd355d382
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-05-08 06:34:41 -0700

    style for product card

--- Frontend/src/Components/CartItems.jsx
+++ Frontend/src/Components/CartItems.jsx
@@ -2,87 +2,122 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import remove_icon from '../assets/remove_icon.png'
 import { BASE_URL } from '../api'
+import pencil_icon from '../assets/pencile.png'
+import delete_icon from '../assets/delete.png'
+
 const CartItems = () => {
-    const {getTotalCartAmount, all_product, cartItems, removeFromCart, increaseQty, decreaseQty } = useContext(ShopContext)
+
+    const {
+        getTotalCartAmount,
+        all_product,
+        cartItems,
+        removeFromCart,
+        increaseQty,
+        decreaseQty
+    } = useContext(ShopContext)
 
     return (
-        <div>
-            <div className='flex items-center justify-between text-gray-500 font-semibold text-sm md:text-xl mx-10 md:mx-30 my-5'>
-                <p >Products</p>
-                <p>Title</p>
-                <p>Price</p>
-                <p>Size</p>
-                <p>Quantity</p>
-                <p>Total</p>
-                <p>Remove</p>
-            </div>
-            <hr className='h-3' />
+        <div className='px-4 md:px-10 lg:px-20 py-10'>
 
-           {all_product.map((e) => {
+            {/* Cart Table*/}
+            <div className='border border-gray-200'>
 
-    return Object.keys(cartItems).map((key) => {
-
-        const productId = key.split("_")[0]
-        const size = key.split("_")[1]
-
-        if (Number(productId) === e.id && cartItems[key] > 0) {
-
-            return (
-                <div
-                    key={key}
-                    className='flex items-center justify-between font-semibold text-gray-500 mx-10 md:mx-30 my-5'
-                >
-                    <img     src={`${BASE_URL}${e.image}`}
- alt='' className='h-30' />
-
-                    <p>{e.name}</p>
-
-                    <p>${e.new_price}</p>
-
-                    <p>Size: {size}</p>
-
-                    <div className='flex items-center gap-2'>
-    
-    {/* minus button */}
-    <button
-        onClick={() => decreaseQty(key)}
-        className='w-8 h-8 border'
-    >
-        -
-    </button>
-
-    {/* quantity */}
-    <button className='w-10 h-8 border'>
-        {cartItems[key]}
-    </button>
-
-    {/* plus button */}
-    <button
-        onClick={() => increaseQty(key)}
-        className='w-8 h-8 border cursor-pointer'
-    >
-        +
-    </button>
-
-</div>
-
-                    <p>
-                        {e.new_price * cartItems[key]}
-                    </p>
-
-                    <img
-                        className='h-6 cursor-pointer'
-                        src={remove_icon}
-                        alt=''
-                        onClick={() => removeFromCart(key)}
-                    />
+                {/* Header*/}
+                <div className='hidden border-b border-gray-200 md:grid md:grid-cols-[2fr_1fr_1fr_1fr_1fr]  text-gray-500 text-lg font-medium '>
+                    <div className='p-6 text-center border-r border-gray-200'>Products</div>
+                    <div className='p-6 text-center border-r border-gray-200'>Price</div>
+                    <div className='p-6 text-center border-r border-gray-200'>Size</div>
+                    <div className='p-6 text-center border-r border-gray-200'>Quantity</div>
+                    <div className='p-6 text-center border-r border-gray-200'>Subtotal</div>
                 </div>
-            )
-        }
 
-        return null
-    })
-})}
+                {all_product.map((e) => {
+
+                    return Object.keys(cartItems).map((key) => {
+
+                        const productId = key.split("_")[0]
+                        const size = key.split("_")[1]
+
+                        if (Number(productId) === e.id && cartItems[key] > 0) {
+
+                            return (
+                                <div
+                                    key={key}
+                                    className='grid grid-cols-1 md:grid-cols-[2fr_1fr_1fr_1fr_1fr] border-b '
+                                >
+
+                                    {/* Product */}
+                                    <div className=' flex gap-5 p-6 border-r border-gray-200'>
+                                        <img src={`${BASE_URL}${e.image}`} alt='' className=' w-28 h-30 object-cover' />
+
+                                        <div className='flex flex-col justify-center'>
+                                            <h1 className='text-lg font-semibold text-gray-800'>{e.name}</h1>
+
+                                            <div className='flex gap-3 mt-3'>
+                                                <button className='text-sm text-gray-500 hover:text-black'>
+                                                    <img src={pencil_icon} alt='' className='h-4 cursor-pointer' />
+                                                </button>
+                                                <button className='text-sm text-gray-500 hover:text-black'>
+                                                    <img src={delete_icon} alt='' className='h-5 cursor-pointer' onClick={() => removeFromCart(key)}
+/>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    {/* Price */}
+                                    <div className=' flex items-center justify-center border-r border-gray-200 p-6'>
+                                        <p className='text-gray-500 text-lg'>RS : {e.new_price}</p>
+                                    </div>
+
+                                    {/* Sizes */}
+                                    <div className=' flex items-center justify-center border-r border-gray-200 p-6'>
+                                        <p className='text-gray-500 mt-1'>Size: {size}</p>
+
+                                    </div>
+
+                                    {/* Quantity */}
+                                    <div className='flex items-center justify-center border-r border-gray-200 p-6'>
+                                        <div className='flex items-center gap-6 border border-gray-400 rounded-full px-5 py-2'>
+
+                                            {/* minus button */}
+                                            <button
+                                                onClick={() => decreaseQty(key)}
+                                                className='cursor-pointer'
+                                            >
+                                                -
+                                            </button>
+
+                                            {/* quantity */}
+                                            <button className='cursor-pointer '>
+                                                {cartItems[key]}
+                                            </button>
+
+                                            {/* plus button */}
+                                            <button
+                                                onClick={() => increaseQty(key)}
+                                                className='cursor-pointer'
+                                            >
+                                                +
+                                            </button>
+
+                                        </div>
+                                    </div>
+
+                                    {/* Subtotal */}
+                                    <div className='flex items-center justify-center p-6'>
+                                    <p className='text-md font-medium text-gray-700'>
+                                        RS : {e.new_price * cartItems[key]}
+                                    </p>
+                                    </div>
+                                </div>
+                            )
+                        }
+
+                        return null
+                    })
+                })}
+            </div>
             <div className='mx-10 md:mx-20 lg:mx-50 xl:mx-80 md:flex justify-between items-center gap-10 text-xl font-semibold mb-20 md:mb-40 mt-20'>
                 <div className='text-2xl mb-5'>
                     <h1 className='text-gray-800 font-bold text-4xl'>Total Carts</h1>
@@ -106,7 +141,7 @@ const CartItems = () => {
 
                 <div>
                     <p className='py-2'>If you have a promo code? enter here.</p>
-                    <input className='h-10 border border-gray-500 rounded-sm pl-3' placeholder='Enter promo code.' /><br/>
+                    <input className='h-10 border border-gray-500 rounded-sm pl-3' placeholder='Enter promo code.' /><br />
                     <button className='bg-gray-300 rounded-md mt-4 px-5 py-2'>Submit</button>
                 </div>
             </div>
