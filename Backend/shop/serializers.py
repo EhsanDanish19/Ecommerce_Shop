aa97feafc827b707686fcb445aae45844eb9490f
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-07-12 03:40:30 -0700

    updates in productDisplay file

--- Backend/shop/serializers.py
+++ Backend/shop/serializers.py
@@ -20,17 +20,14 @@ class RegisterSerializer(serializers.ModelSerializer):
             password= validated_data['password']
         )
         return user
-
+    
 class ProductSerializer(serializers.ModelSerializer):
     tags = serializers.StringRelatedField(many=True)
     sizes = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Product
-        fields = '__all__'
-    def get_sizes(self, obj):
-        if isinstance(obj.sizes, str):
-            return [s.strip() for s in obj.sizes.split(",")]
+        fields = "__all__"
 
 
 class CartSerializer(serializers.ModelSerializer):
