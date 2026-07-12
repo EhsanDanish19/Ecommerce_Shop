fd4cf5301b3e7ae421265ad15a800e2ba780b64a
Ehsan Ali <ehsanpkd.19@gmail.com>
2026-07-12 02:26:35 -0700

    updates in my orders.jsx file

--- Frontend/src/Pages/My_Orders.jsx
+++ Frontend/src/Pages/My_Orders.jsx
@@ -6,55 +6,65 @@ const My_Orders = () => {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        
-        const fetchOrders = async ()=> {
 
-            try{
-            const token = localStorage.getItem("token");
+        const fetchOrders = async () => {
 
-            const res = await axios.get(
-                `${BASE_URL}/api/my_orders`,
-                {
-                    headers:{
-                        Authorization:`Bearer ${token}`
+            try {
+                const token = localStorage.getItem("token");
+
+                const res = await axios.get(
+                    `${BASE_URL}/api/my_orders`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
                     }
-                }
-            );
-            
-            console.log("Orders:", res.data);
+                );
 
-            setOrders(res.data)
-            
-        } catch (error) {
-            console.log(error.response)
+                setOrders(res.data)
+
+            } catch (error) {
+                console.log(error.response)
+            }
         }
-    }
         fetchOrders();
 
-    },[]);
+    }, []);
 
 
-  return (
-    <div className=''>
-    <h1 className=''>
-        My orders
-    </h1>
+    return (
+        <div className=''>
+            <h1 className=' text-center font-bold text-3xl lg:text-4xl py-10 bg-gray-200 mb-3'>
+                My Orders
+            </h1>
 
-    {
-        orders.map(order=>(
-            <div key={order.id} className=''>
-
-            <h2>Order #{order.id}</h2>
-            <p>Total : Rs {order.total_amount}</p>
-            <p>Payment : {order.payment_method}</p>
-            <p>Status : {order.status} </p>
-            <p>Date : {order.created_at} </p>
-
+            <div className='hidden md:grid grid-cols-5 md:mx-5 lg:mx-20 py-5 text-center font-bold border border-gray-300'>
+                <div>Id</div>
+                <div>Total</div>
+                <div>Payment</div>
+                <div>Status</div>
+                <div>Date</div>
             </div>
-        ))
-    }      
-    </div>
-  )
+
+            {orders.length === 0 ? (
+                <h2 className='text-center text-gray-500 text-3xl mt-10'>
+                    No Orders Found.
+                </h2>
+            ) : (
+                orders.map(order => (
+                    <div key={order.id} className='grid grid-cols-1 md:grid-cols-5 mx-5 md:mx-5 lg:mx-20 py-5 mb-4 md:mb-0 md:pl-0 pl-10 md:text-center border md:border-t-0 border-gray-300'>
+
+                        <h2><span className='md:hidden font-bold'>Id : </span>{order.id}</h2>
+                        <p><span className='md:hidden font-bold'>Total Amount : </span> {order.total_amount}</p>
+                        <p><span className='md:hidden font-bold'>payment Method  : </span> {order.payment_method}</p>
+                        <p><span className='md:hidden font-bold'>Status : </span> {order.status} </p>
+                        <p><span className='md:hidden font-bold'>Date : </span> {order.created_at} </p>
+
+                    </div>
+                ))
+            )}
+        </div>
+    )
 }
 
 export default My_Orders
